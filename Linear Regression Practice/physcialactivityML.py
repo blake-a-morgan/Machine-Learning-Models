0ecae5d4b2418be96f3cab0ad2aba7cbaf361413
Blake Morgan <blakemorgan@gmail.com>
2023-01-06 02:50:12 -0600

    2023 01 06 Dataframes added

--- Linear Regression Practice/physcialactivityML.py
+++ Linear Regression Practice/physcialactivityML.py
@@ -11,11 +11,25 @@ from sklearn import metrics
 
 # %%
 #Reading data into DataFrame and displaying it
-nutrition = pd.read_csv('NutritionData.csv')
-nutrition
+df = pd.read_csv('NutritionData.csv')
+df.info()
+df.head()
 
+# %%
+##Drop unneccessary tables
+df.drop(['Low_Confidence_Limit','High_Confidence_Limit ','YearEnd','Topic','Class','Datasource','Data_Value_Unit','QuestionID','ClassID','TopicID','DataValueTypeID','Data_Value_Type','Data_Value_Footnote_Symbol','Data_Value_Footnote','StratificationCategoryId1','StratificationID1'],axis=1,inplace=True)
+df.info()
+
+#%%
+df.info()
+# %%
+#Create new data frames to separate by gender,education level, and income
+dfgend= df[(df['Stratification1']=='Male')|(df['Stratification1']=='Female')]
+dfedu = df[df['StratificationCategory1']=='Education']
+dfinc = df[df['StratificationCategory1']=='Income']
 
 
 # %%
-sns.pairplot(data=nutrition)
-# %%
+##Some Visualizations of the data
+sns.pairplot(data=df)
+
