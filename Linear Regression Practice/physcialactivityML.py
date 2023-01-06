d221518cbb837986932b406fa33b58133cdf7d33
Blake Morgan <blakemorgan@gmail.com>
2023-01-06 01:24:32 -0600

    New work

---
+++ Linear Regression Practice/physcialactivityML.py
@@ -0,0 +1,14 @@
+#%%
+##Imports
+#%#
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+
+# %%
+
+data = pd.read_csv('')
